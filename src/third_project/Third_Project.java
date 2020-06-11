987597eb2acdf438e4d0136db14607be8cfec8cf
BonyGeorge <abanoubgeorge16@gmail.com>
2020-06-11 21:45:48 +0200

    Added Bipartite-Matching method

--- src/third_project/Third_Project.java
+++ src/third_project/Third_Project.java
@@ -4,16 +4,27 @@ package third_project;
  *
  * @author abanoublamie
  */
-// A Java program to find maximal 
-// Bipartite matching. 
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
+   Problem :
+   A matching in a Bipartite graph is a set of the edges chosen in such a
+   way that no two edges share an endpoint. A maximum matching is a
+   matching of maximum size (maximum number of edges). In a
+   maximum matching, if any edge is added to it, it is no longer a
+   matching. There can be more than one maximum matchings for a given
+   Bipartite graph.
+   For example, consider the following problem:
+   There are M job applicants and N jobs. Each applicant has a subset of
+   jobs that he/she is interested in. Each job opening can only accept one
+   applicant and a job applicant can be appointed for only one job. Find an
+   assignment of jobs to applicants in such that as many applicants as
+   possible get jobs.
+
  */
 class GFG 
 {
@@ -24,7 +35,7 @@ class GFG
     // A DFS based recursive function that  
     // returns true if a matching for  
     // vertex u is possible 
-    boolean bpm(boolean bpGraph[][], int Applicants_Vertex, boolean[] viewed, int matchR[])
+    public boolean Bipartite_Matching(boolean bpGraph[][], int Applicants_Vertex, boolean[] viewed, int matchR[])
     {
         // Try every job one by one 
         for (int Jobs_Vertex = 0; Jobs_Vertex < Jobs_Number; Jobs_Vertex++)
@@ -42,8 +53,9 @@ class GFG
                 // is matchR[v]) has an alternate job available. 
                 // Since v is marked as visited in the  
                 // above line, matchR[v] in the following 
-                // recursive call will not get job 'v' again 
-                if (matchR[Jobs_Vertex] < 0 || bpm(bpGraph, matchR[Jobs_Vertex], viewed, matchR))
+                // recursive call will not get job 'v' again
+                
+                if (matchR[Jobs_Vertex] < 0 || Bipartite_Matching(bpGraph, matchR[Jobs_Vertex], viewed, matchR))
                 {
                     matchR[Jobs_Vertex] = Applicants_Vertex;
                     return true;
@@ -55,31 +67,36 @@ class GFG
 
     // Returns maximum number  
     // of matching from M to N 
-    int Maximum_Bipartite_Matching(boolean[][] Graph) {
+    public int Maximum_Bipartite_Matching(boolean[][] Graph)
+    {
         // An array to keep track of the  
         // applicants assigned to jobs.  
         // The value of matchR[i] is the  
         // applicant number assigned to job i,  
         // the value -1 indicates nobody is assigned. 
-        int matchR[] = new int[Jobs_Number];
+        int[] Matched_Applicant = new int[Jobs_Number];
 
         // Initially all jobs are available 
-        for (int i = 0; i < Jobs_Number; ++i) {
-            matchR[i] = -1;
+        for (int i = 0; i < Jobs_Number; ++i)
+        {
+            Matched_Applicant[i] = -1;
         }
 
         // Count of jobs assigned to applicants 
         int result = 0;
-        for (int u = 0; u < Applicants_Number; u++) {
+        for (int u = 0; u < Applicants_Number; u++)
+        {
             // Mark all jobs as not seen  
             // for next applicant. 
-            boolean seen[] = new boolean[Jobs_Number];
-            for (int i = 0; i < Jobs_Number; ++i) {
-                seen[i] = false;
+            boolean[] viewed = new boolean[Jobs_Number];
+            for (int i = 0; i < Jobs_Number; ++i)
+            {
+                viewed[i] = false;
             }
 
-            // Find if the applicant 'u' can get a job 
-            if (bpm(Graph, u, seen, matchR)) {
+            // It checks if the applicant is suitable for the job.
+            if (Bipartite_Matching(Graph, u, viewed, Matched_Applicant))
+            {
                 result++;
             }
         }
