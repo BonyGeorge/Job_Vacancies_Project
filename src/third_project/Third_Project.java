7577f5b59ddd4f4886d03938835376cae7d0be99
BonyGeorge <abanoubgeorge16@gmail.com>
2020-06-14 03:50:12 +0200

    Dynamic Jobs & Applicants

--- src/third_project/Third_Project.java
+++ src/third_project/Third_Project.java
@@ -12,6 +12,7 @@ import java.io.*;
 /*
 
    Problem :
+
    A matching in a Bipartite graph is a set of the edges chosen in such a
    way that no two edges share an endpoint. A maximum matching is a
    matching of maximum size (maximum number of edges). In a
@@ -26,38 +27,48 @@ import java.io.*;
    possible get jobs.
 
  */
-class GFG 
+class Job_Vacancies 
 {
 
-    static final int Applicants_Number = 6;
-    static final int Jobs_Number = 6;
+    static int Applicants_Number;
+    static int Jobs_Number;
 
-    // A DFS based recursive function that  
-    // returns true if a matching for  
-    // vertex u is possible 
-    public boolean Bipartite_Matching(boolean bpGraph[][], int Applicants_Vertex, boolean[] viewed, int matchR[])
+    public Job_Vacancies()
     {
-        // Try every job one by one 
+        Applicants_Number = 0;
+        Jobs_Number = 0;
+    }    
+    
+    // Functions to get the number of jobs available and number of submitted applicants
+    public int insert_Count_Applicants(int Number)
+    {
+        return this.Applicants_Number = Number;
+    }        
+    
+    public int insert_Count_Jobs(int Number)
+    {
+        return this.Jobs_Number = Number;
+    }        
+    
+    // Function to check matching between verticies.
+    public boolean Bipartite_Matching(boolean[][] Bipartite_Graph, int Applicants_Vertex, boolean[] viewed, int[] Matched)
+    {
         for (int Jobs_Vertex = 0; Jobs_Vertex < Jobs_Number; Jobs_Vertex++)
         {
-            // If applicant u is interested  
-            // in job v and v is not visited 
-            if (bpGraph[Applicants_Vertex][Jobs_Vertex] && !viewed[Jobs_Vertex]) {
-
-                // The application was seen by the 
+            // Checks if the applicant submitted fo this job or not he must not be in another job.
+            if (Bipartite_Graph[Applicants_Vertex][Jobs_Vertex] && !viewed[Jobs_Vertex]) 
+            {
                 viewed[Jobs_Vertex] = true;
-
-                // If job 'v' is not assigned to 
-                // an applicant OR previously 
-                // assigned applicant for job v (which 
-                // is matchR[v]) has an alternate job available. 
-                // Since v is marked as visited in the  
-                // above line, matchR[v] in the following 
-                // recursive call will not get job 'v' again
                 
-                if (matchR[Jobs_Vertex] < 0 || Bipartite_Matching(bpGraph, matchR[Jobs_Vertex], viewed, matchR))
+                /*
+                    If the job isn't assigned to any applicant
+                    then the applicant will be assigned for the 
+                    job.
+                */
+                
+                if (Matched[Jobs_Vertex] < 0 || Bipartite_Matching(Bipartite_Graph, Matched[Jobs_Vertex], viewed, Matched))
                 {
-                    matchR[Jobs_Vertex] = Applicants_Vertex;
+                    Matched[Jobs_Vertex] = Applicants_Vertex;
                     return true;
                 }
             }
@@ -68,38 +79,35 @@ class GFG
     // Returns maximum number  
     // of matching from M to N 
     public int Maximum_Bipartite_Matching(boolean[][] Graph)
-    {
-        // An array to keep track of the  
-        // applicants assigned to jobs.  
-        // The value of matchR[i] is the  
-        // applicant number assigned to job i,  
-        // the value -1 indicates nobody is assigned. 
+    { 
         int[] Matched_Applicant = new int[Jobs_Number];
 
-        // Initially all jobs are available 
+        // It initialize all the jobs by available for any applicant to submit in it.
         for (int i = 0; i < Jobs_Number; ++i)
         {
             Matched_Applicant[i] = -1;
         }
-
-        // Count of jobs assigned to applicants 
+ 
         int result = 0;
-        for (int u = 0; u < Applicants_Number; u++)
+        
+        // We count here how many jobs the applicant submitted in.
+        for (int Vertex_U = 0; Vertex_U < Applicants_Number; Vertex_U++)
         {
-            // Mark all jobs as not seen  
-            // for next applicant. 
+           
             boolean[] viewed = new boolean[Jobs_Number];
+            
             for (int i = 0; i < Jobs_Number; ++i)
             {
                 viewed[i] = false;
             }
-
+            
             // It checks if the applicant is suitable for the job.
-            if (Bipartite_Matching(Graph, u, viewed, Matched_Applicant))
+            if (Bipartite_Matching(Graph, Vertex_U, viewed, Matched_Applicant))
             {
                 result++;
             }
         }
+        
         return result;
     }
 }
@@ -129,7 +137,15 @@ public class Third_Project {
                 false, false, true}};
 
         // Instance of 
-        GFG m = new GFG();
+        Job_Vacancies m = new Job_Vacancies();
+        Scanner number = new Scanner(System.in);
+        
+        System.out.println("Enter Number of Applicants : ");
+        m.insert_Count_Applicants(number.nextInt());
+        
+        System.out.println("Enter Number of Jobs : ");
+        m.insert_Count_Jobs(number.nextInt());
+        
         System.out.println("Maximum number of Applicants that are suitble for the job : " + m.Maximum_Bipartite_Matching(Graph));
     }
 }
